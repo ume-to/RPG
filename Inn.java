620e33a16a937b3a40f9087e6fbe960100b3d7b4
ume-to <takuto8101@gmail.com>
2019-04-09 22:42:38 +0900

    宿屋クラスを追加

---
+++ Inn.java
@@ -0,0 +1,11 @@
+
+public class Inn{
+
+	//主人公のHPを回復させる
+
+	public static void heal(Hero h){
+		System.out.println(h.getName() + "はHPが最大まで回復した");
+		System.out.println(h.getHp()+"→"+h.getMaxHp());
+		h.setHp(h.getMaxHp());
+	}
+}
